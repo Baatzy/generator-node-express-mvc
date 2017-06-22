93e6c56f9b9187edfb1dabaf1c494ef62efbd4d8
Wes Reid <bwreid@gmail.com>
2017-06-22 13:51:30 -0700

    Basic express generator
    - Add express generator as part of template
    - Use ejs for views
    - Install with yarn and do not use npm or bower

--- generators/app/index.js
+++ generators/app/index.js
@@ -7,30 +7,22 @@ module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
     this.log(yosay(
-      'Welcome to the groovy ' + chalk.red('generator-node-express-mvc') + ' generator!'
+      'Welcome to the ' + chalk.red('node-express-mvc') + ' generator!'
     ));
-
-    const prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
-
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-    });
   }
 
   writing() {
     this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+      this.templatePath('**/*'),
+      this.destinationPath('example/.')
     );
   }
 
   install() {
-    this.installDependencies();
+    this.installDependencies({
+      bower: false,
+      npm: false,
+      yarn: true
+    });
   }
 };
